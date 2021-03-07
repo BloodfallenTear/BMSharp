af40289a802333152813e0bc46c4fd4706a3c732
BloodfallenTear <bloodfallentear@hotmail.com>
2021-03-07 14:20:28 +0100

    Add project files.

---
+++ BMSharp/BattleMetrics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+
+namespace BMSharp
+{
+    public class BattleMetrics
+    {
+        internal static HttpClient HttpClient { get; private set; }
+
+        private String BearerToken { get; set; }
+
+        public BattleMetrics() 
+        {
+            BattleMetrics.HttpClient = new HttpClient();
+            BattleMetrics.HttpClient.DefaultRequestHeaders.ConnectionClose = true;
+        }
+
+        public BattleMetrics(String BearerToken) : this()
+        {
+            this.BearerToken = BearerToken;
+        }
+    }
+}
